472a21ab6b6f66ae1b1cea024cda347b84aaec35
Jintan09 <sfritsch09@gmail.com>
2020-12-19 18:31:20 +0100

    navbar changes

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,8 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import { BackgroundImg, Nav, NavIcon, NavItem, NavLink, NavLogo, NavMenu } from './navbar.styles';
+import { BiUser } from 'react-icons/bi';
+import { FaTimes, FaBars } from 'react-icons/fa';
+import {
+	LogoContainer,
+	Nav,
+	NavIcon,
+	NavItem,
+	NavLink,
+	NavLogo,
+	NavMenu,
+	NavTools,
+	NavbarContainer,
+	MobileIcon,
+} from './navbar.styles';
 
 const Navbar = () => {
 	const [scroll, setScroll] = useState(0);
+	const [click, setClick] = useState(false);
+
+	const handleClick = () => setClick(!click);
 
 	useEffect(() => {
 		document.addEventListener('scroll', () => {
@@ -14,37 +30,50 @@ const Navbar = () => {
 	});
 	return (
 		<div>
-			<Nav className={scroll ? 'sticky' : 'is-hidden'}>
-				<NavLogo to="/">
-					<NavIcon className={scroll ? 'sticky' : ''} />
-				</NavLogo>
-				<NavMenu>
-					<NavItem>
-						<NavLink className={scroll ? 'sticky' : ''} to="/">
-							HOME
-						</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink className={scroll ? 'sticky' : ''} to="/">
-							ÜBER MICH
-						</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink className={scroll ? 'sticky' : ''} to="/">
-							EQUIPMENT
-						</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink className={scroll ? 'sticky' : ''} to="/">
-							PREISE
-						</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink className={scroll ? 'sticky' : ''} to="/">
-							ANFRAGE
-						</NavLink>
-					</NavItem>
-				</NavMenu>
+			<Nav className={scroll ? 'sticky' : ''}>
+				<NavbarContainer>
+					<LogoContainer>
+						<NavLogo>
+							<NavIcon className={scroll ? 'sticky' : ''} />
+						</NavLogo>
+					</LogoContainer>
+					<MobileIcon onClick={handleClick}>{click ? <FaTimes /> : <FaBars />}</MobileIcon>
+					<NavMenu conClick={handleClick} click={click}>
+						<NavItem>
+							<NavLink className={scroll ? 'sticky' : ''} to="/">
+								HOME
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink className={scroll ? 'sticky' : ''} to="/">
+								MUSIC
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink className={scroll ? 'sticky' : ''} to="/">
+								ÜBER MICH
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink className={scroll ? 'sticky' : ''} to="/">
+								EQUIPMENT
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink className={scroll ? 'sticky' : ''} to="/">
+								PREISE
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink className={scroll ? 'sticky' : ''} to="/">
+								ANFRAGE
+							</NavLink>
+						</NavItem>
+					</NavMenu>
+					<NavTools className={scroll ? 'sticky' : ''}>
+						<BiUser />
+					</NavTools>
+				</NavbarContainer>
 			</Nav>
 		</div>
 	);
