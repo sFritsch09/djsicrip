6cac20b77ecae8585ee914a0348dd35187b84d7e
Jintan09 <sfritsch09@gmail.com>
2021-03-11 17:12:32 +0100

    update

--- src/pages/equipment/equipment.styles.js
+++ src/pages/equipment/equipment.styles.js
@@ -9,17 +9,20 @@ export const Section = styled.div`
 	justify-content: center;
 	position: relative;
 	width: 100%;
+	touch-action: none;
 `;
 
 export const Section2 = styled.div`
 	justify-content: center;
 	position: relative;
 	width: 100%;
+	touch-action: none;
 `;
 export const Section3 = styled.div`
 	justify-content: center;
 	position: relative;
 	width: 100%;
+	touch-action: none;
 `;
 export const Spacer = styled.div`
 	width: 100%;
